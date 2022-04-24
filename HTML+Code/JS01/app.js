c7db57621e28f4fa911d4bb86be39d1cbb8e2c2e
liu-yi-fan <67992696+liu-yi-fan@users.noreply.github.com>
2022-04-24 19:52:09 +0800

    html/css

---
+++ HTML+Code/JS01/app.js
@@ -0,0 +1,71 @@
+let input = prompt("Enter a month.");
+
+switch(input){
+    case "January":
+        alert("一月");
+        break;
+    case "February":
+        alert("二月");
+        break;
+    case "March":
+        alert("三月");
+        break;
+    case "April":
+        alert("四月");
+        break;
+    case "May":
+        alert("五月");
+        break;
+    case "June":
+        alert("六月");
+        break;
+    case "July":
+        alert("七月");
+        break;
+    case "August":
+        alert("八月");
+        break;
+    case "Septemper":
+        alert("九月");
+        break;
+    case "October":
+        alert("十月");
+        break;
+    case "November":
+        alert("十一月");
+        break;
+    case "December":
+        alert("十二月");
+        break;
+    default:
+        alert("無法判定");
+        break;
+}
+
+// if (input == "January"){
+//     alert("一月");
+// }else if (input == "February"){
+//     alert("二月");
+// }else if (input == "March"){
+//     alert("三月");
+// }else if (input == "April"){
+//     alert("四月");
+// }else if (input == "May"){
+//     alert("五月");
+// }else if (input == "June"){
+//     alert("六月");
+// }else if (input == "July"){
+//     alert("七月");
+// }else if (input == "August"){
+//     alert("八月");
+// }else if (input == "Septemper"){
+//     alert("九月");
+// }else if (input == "October"){
+//     alert("十月");
+// }else if (input == "November"){
+//     alert("十一月");
+// }else if (input == "December"){
+//     alert("十二月");
+// }else{
+//     alert("無法判定");
+// }
---
+++ HTML+Code/portfolio/Hawaii Project/app.js
@@ -0,0 +1,19 @@
+let header = document.querySelector("header");
+let headerAnchor = document.querySelectorAll("header nav ul li a");
+console.log(headerAnchor);
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset !=0){
+        header.style.backgroundColor ="rgba(0,0,0,0.9)";
+        header.style.color = "white";
+        headerAnchor.forEach(a => {
+            a.style.color= "white";
+        })
+
+    } else {
+        header.style = "";
+        headerAnchor.forEach(a => {
+            a.style.color= "#09777D";
+        })
+    }
+})
